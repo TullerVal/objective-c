8aa3a3db3a6656f8c1a41ba52ad5226951d8b8b4
TullerVal <travis@travis-ci.org>
2014-03-28 19:59:52 +0200

    Added build number to path

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -10,8 +10,8 @@ git clone --quiet --branch=gh-pages https://cf75effcf9d0ba6e5fdb3d7d6158f1a8e405
 
 date
 cd gh-pages
-mkdir `date +%Y.%m.%d_%H.%m`
-cd `date +%Y.%m.%d_%H.%m`
+mkdir `date +%Y.%m.%d_%H.%m_$TRAVIS_BUILD_NUMBER`
+cd    `date +%Y.%m.%d_%H.%m_$TRAVIS_BUILD_NUMBER`
 ls -all
 rm -R *
 cp -Rf $HOME/coverage/* .
@@ -19,7 +19,7 @@ cd ..
 
 #add, commit and push files
 git add -f .
-git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages (branch $TRAVIS_BRANCH, commit $TRAVIS_COMMIT)"
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages (branch $TRAVIS_BRANCH, commit $TRAVIS_COMMIT (build number $TRAVIS_BUILD_NUMBER))”
 git push -fq origin gh-pages
 
 echo -e "Done magic with coverage\n"
