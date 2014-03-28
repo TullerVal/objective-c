d8ee3b1f9be41121781e9a24611ec072c0cc5c4c
TullerVal <travis@travis-ci.org>
2014-03-28 14:42:37 +0200

    Test

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -6,14 +6,14 @@ cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "TullerVal"
 
-git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/TullerVal/SharedTravisLogs.git gh-pages
+git clone --quiet --branch=gh-pages https://cf75effcf9d0ba6e5fdb3d7d6158f1a8e4054e7e@github.com/TullerVal/SharedTravisLogs.git gh-pages
 
 cd gh-pages
 cp -Rf $HOME/coverage/* .
 
 #add, commit and push files
 git add -f .
-git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages (branch $TRAVIS_BRANCH)
 git push -fq origin gh-pages
 
 echo -e "Done magic with coverage\n"
