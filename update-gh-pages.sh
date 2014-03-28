8ee781883f2f43046500076b584b6e7a9c93a403
TullerVal <travis@travis-ci.org>
2014-03-28 19:17:30 +0200

    Added folder with time

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -10,9 +10,12 @@ git config --global user.name "TullerVal"
 git clone --quiet --branch=gh-pages https://cf75effcf9d0ba6e5fdb3d7d6158f1a8e4054e7e@github.com/TullerVal/SharedTravisLogs.git gh-pages
 
 cd gh-pages
+mkdir `date +%Y.%m.%d_%H.%m`
+cd `date +%Y.%m.%d_%H.%m`
 ls -all
 rm -R *
 cp -Rf $HOME/coverage/* .
+cd ..
 
 #add, commit and push files
 git add -f .
