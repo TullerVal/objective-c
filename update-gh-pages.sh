4ba7c453102997b1e3b7ceebaf5962ce6d513fa5
TullerVal <travis@travis-ci.org>
2014-03-28 19:02:49 +0200

    Clear prevision result

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -11,12 +11,12 @@ git clone --quiet --branch=gh-pages https://cf75effcf9d0ba6e5fdb3d7d6158f1a8e405
 
 cd gh-pages
 ls -all
-rm -R $HOME/coverage/*
+rm -R *
 cp -Rf $HOME/coverage/* .
 
 #add, commit and push files
 git add -f .
-git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages (branch $TRAVIS_BRANCH, commit $TRAVIS_COMMIT, %{message})"
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages (branch $TRAVIS_BRANCH, commit $TRAVIS_COMMIT)"
 git push -fq origin gh-pages
 
 echo -e "Done magic with coverage\n"
